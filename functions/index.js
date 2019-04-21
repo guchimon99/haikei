7771078bb562b4dd4a8478db28a76d13c128db0f
guchimon99 <guchimon99@gmail.com>
2019-04-21 16:51:58 +0900

    図の編集

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/v/:id', (req, res) => {
-  res.render('pages/user', {title: "Hello"})
+  res.render('pages/user', {id: req.params.id })
 })
 
 exports.app = functions.https.onRequest(app);
